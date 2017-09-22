e41b7d566aaf2ac014df2ab9d864afe3ee1922fb
Jeff Klouda <jklouda1@nd.edu>
2017-09-22 16:00:32 -0400

    Added class prototype for scheduler

--- pq.h
+++ pq.h
@@ -6,6 +6,8 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 enum Policy { fifo, rdrn, mlfq };
 enum proc_status { running, ready, blocked };
 
@@ -14,7 +16,33 @@ struct Process {
     vector<string> command;
     pid_t pid;
     proc_status status;
-    int priority;
+    int priority;           //  MLFQ priority
+    unsigned int utime;     //  User mode CPU time, in clock ticks
+    unsigned int stime;     //  Kernel mode CPU time, in clock ticks
+    unsigned int cutime;    //  Waited-for children's user mode CPU time
+    unsigned int cstime;    //  Waited-for children's kernel mode CPU time
+    unsigned int starttime; //  Time when process started in clock ticks
+    unsigned int schedtime; //  Time when process was first scheduled
 
+};
 
+class Scheduler {
+
+    public:
+        Scheduler (Policy, int ncpus, int tmslice);
+        ~Scheduler ();
+        Process getProcess(pid_t);         
+        int addJob (string);                        // return error?
+        int executeJob (string);
+        int pauseProcess (Process);
+        int resumeProcess (Process);
+        int terminateProcess (Process);
+        void run();                                 // run scheduler
+        
+
+    private:
+        int nCPUS;
+        int timeSlice;
+        Policy policy;
+        vector<Process> processTable;
 };
